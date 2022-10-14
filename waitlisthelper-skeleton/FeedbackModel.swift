413c5c675845fb7378da62714dbe6ff449874c81
Carolyn Liu <caliu@berkeley.edu>
2022-10-13 22:57:14 -0700

    finished hw4

---
+++ waitlisthelper-skeleton/FeedbackModel.swift
@@ -0,0 +1,16 @@
+//
+//  FeedbackModel.swift
+//  waitlisthelper-skeleton
+//
+//  Created by Carolyn Liu on 10/13/22.
+//
+
+import Foundation
+let feedbackModel =
+    [(90, "You're good 😄"),
+     (60, "You're probably okay ☺️"),
+     (40, "Idk, it's like 50-50 😐"),
+     (20, "You're probably not getting off 😕"),
+     (0, "Rip 😔")]
+
+var test = "test"
